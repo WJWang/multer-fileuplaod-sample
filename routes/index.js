78263d4dc415a97a4dc9b0f82c065ba7a4ad5020
William Wang <shadow25251@gmail.com>
2017-02-05 23:10:02 +0800

    init

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,30 @@
 var express = require('express');
 var router = express.Router();
+var multer  = require('multer')
+var path = require('path');
+var storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, path.join(__dirname, '../public/images/uploads/') )
+  },
+  filename: function (req, file, cb) {
+    var ext = file.mimetype.split('/')[1];
+    cb(null, file.fieldname + '-' + Date.now() + '.' + ext)
+  }
+})
+
+var upload = multer({ storage: storage })
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.post('/file', upload.single('photo'), (req, res) => {
+  const url = req.file.path.split('public/')[1];
+  res.render('photo', {
+    title: req.body.title,
+    img: url
+  })
+})
 module.exports = router;
